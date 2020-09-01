b5c4c6c07cef6adb040b37f978d5c673704e4397
M3D5 <pablohdzjr@gmail.com>
2020-09-01 16:08:55 -0500

    created routes for pages

--- src/components/main.js
+++ src/components/main.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Landing from "./landing";
+import AboutMe from "./aboutme";
+import Projects from "./projects";
+import Resume from "./resume";
+import Contact from "./contact";
 import { Switch, Route } from "react-router-dom";
 
 const Main = () => (
   <Switch>
     <Route exact path="/" component={Landing} />
+    <Route exact path="/aboutme" component={AboutMe} />
+    <Route exact path="/projects" component={Projects} />
+    <Route exact path="/resume" component={Resume} />
+    <Route exact path="/contact" component={Contact} />
   </Switch>
 );
 
