73626fd73b29bdfc144630c1781cdc9f10369a7b
M3D5 <pablohdzjr@gmail.com>
2020-09-01 20:26:20 -0500

    added dummy cards to Prjects section

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,13 +1,127 @@
 import React, { Component } from "react";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardText,
+  CardActions,
+  CardMenu,
+  Button,
+  IconButton,
+} from "react-mdl";
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+        <div className="projects-grid">
+          {/*Project 1 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                backdground: "URL() center/cover",
+              }}
+            >
+              Project 1
+            </CardTitle>
+            <CardText>Project Description</CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/*Project 2 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                backdground: "URL() center/cover",
+              }}
+            >
+              Project 2
+            </CardTitle>
+            <CardText>Project Description</CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/*Project 3 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                backdground: "URL() center/cover",
+              }}
+            >
+              Project 3
+            </CardTitle>
+            <CardText>Project Description</CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <h1>This is Group Projects</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div>
+          <h1>This is Side Projects</h1>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>Projects Page</h1>
+      <div className="category-tabs">
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={(tabId) => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>Projects</Tab>
+          <Tab>Group Projects</Tab>
+          <Tab>Side Projects</Tab>
+        </Tabs>
+
+        <Grid>
+          <Cell col={12}>
+            <div className="content">{this.toggleCategories()} </div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
 }
-
 export default Projects;
