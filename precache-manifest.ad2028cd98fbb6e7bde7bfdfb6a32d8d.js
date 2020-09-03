c3b5d78fd5cb72db189b4ba18ffeae90a9b70b21
M3D5 <pablohdzjr@gmail.com>
2020-09-02 22:54:19 -0500

    Updates

---
+++ precache-manifest.ad2028cd98fbb6e7bde7bfdfb6a32d8d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb5fd730c60622ab52288286a824fac",
+    "url": "/my-react-portfolio/index.html"
+  },
+  {
+    "revision": "5c92a890425ec3702256",
+    "url": "/my-react-portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "04f23af7e2fdc7cec2cc",
+    "url": "/my-react-portfolio/static/css/main.eac51804.chunk.css"
+  },
+  {
+    "revision": "5c92a890425ec3702256",
+    "url": "/my-react-portfolio/static/js/2.ddcf1c43.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/my-react-portfolio/static/js/2.ddcf1c43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04f23af7e2fdc7cec2cc",
+    "url": "/my-react-portfolio/static/js/main.4994e25a.chunk.js"
+  },
+  {
+    "revision": "832cd5bd3ab6113fb040",
+    "url": "/my-react-portfolio/static/js/runtime-main.047d202f.js"
+  },
+  {
+    "revision": "bb9bbd91b8d9d3d605e246e9eed89f7a",
+    "url": "/my-react-portfolio/static/media/BrewFinder.bb9bbd91.png"
+  },
+  {
+    "revision": "6ff49fc3de46b690ee62d45cbed3c8ea",
+    "url": "/my-react-portfolio/static/media/BurgerApp.6ff49fc3.png"
+  },
+  {
+    "revision": "e9f7a6a7f66200b4366141390de33f28",
+    "url": "/my-react-portfolio/static/media/CodeQuiz.e9f7a6a7.png"
+  },
+  {
+    "revision": "c01337e66fdd5a943c90f4ae55a2f3d0",
+    "url": "/my-react-portfolio/static/media/EntertainmentReviewer.c01337e6.png"
+  },
+  {
+    "revision": "2641a2a0c55fc70f59d6577c7a1f2df2",
+    "url": "/my-react-portfolio/static/media/PasswordGenerator.2641a2a0.png"
+  },
+  {
+    "revision": "f4e696cba5c81f7d9dabca5ce066416e",
+    "url": "/my-react-portfolio/static/media/ReadMeGen.f4e696cb.png"
+  },
+  {
+    "revision": "7f537a1206026c26e4d5f0b19abcba38",
+    "url": "/my-react-portfolio/static/media/headshot.7f537a12.jpg"
+  },
+  {
+    "revision": "9880cd40ab1441f80a1d2ad0b5b794a9",
+    "url": "/my-react-portfolio/static/media/pablo-logo.9880cd40.png"
+  }
+]);
